38d2d5ada4f551069c3b6c50f50afca320303fd1
Moritz Kirmse <moritz.kirmse@camptocamp.com>
2020-04-10 12:40:15 +0200

    streamline and clean up code

--- groupLayers/plugin.py
+++ groupLayers/plugin.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 import os
-from collections import defaultdict
 from qgis.PyQt.QtWidgets import QAction
 from qgis.PyQt.QtGui import QIcon
-from qgis.core import QgsLayerTree, QgsLayerTreeGroup
-from qgis.core import QgsMapLayerType
-from qgis.core import QgsWkbTypes
+from qgis.core import QgsLayerTree, QgsLayerTreeGroup, QgsLayerTreeLayer
 from.groupTypes import groupHierarchy
+
 
 class MainPlugin(object):
     def __init__(self, iface):
@@ -24,9 +22,11 @@ class MainPlugin(object):
         self.action.setWhatsThis("Group/ungroup layers by type")
         self.action.setStatusTip("Group/ungroup layers by type")
         self.action.setCheckable(True)
-        self.action.setChecked(False)
-        print(self.action.isChecked())
         self.action.triggered.connect(self.run)
+        # the button status could be used, but it is already
+        # changed when run method is called, so this is ambiguous
+        # therefore a dedicated boolean status is used
+        self.grouped = False
 
         # add toolbar button and menu item
         self.iface.addToolBarIcon(self.action)
@@ -39,9 +39,7 @@ class MainPlugin(object):
 
     def run(self):
         # create and show a configuration dialog or something similar
-        print("groupPlugin: run called!")
-        print(self.action.isChecked())
-        if not self.action.isChecked():
+        if self.grouped:
             self.groupToTree()
             self.grouped = False
         else:
@@ -56,42 +54,19 @@ class MainPlugin(object):
             else:
                 tree[k] = []
 
-    def defaultTree(self):
-        return {
-            'vector': {
-                'point': [],
-                'line': [],
-                'polygon': []
-            },
-            'raster': {
-                'wms': [],
-                'wfs': []
-            }
-        }
-
     def treeToGroup(self):
-        # newTree = self.defaultTree()
         self.newTree = {}
         self.initTreeRec(groupHierarchy['values'], self.newTree)
-        print(self.newTree)
         layerTree = self.iface.layerTreeCanvasBridge().rootGroup()
         self.oldTree = layerTree.clone()
         self.parseTreeRec(layerTree)
-        #self.parseTreeRecDef(layerTree, groupHierarchy)
-        print(self.newTree)
         oldLen = len(layerTree.children())
 
-        # ll = self.treeToLayers(self.newTree)
-        # print (ll)
-        # for l in ll:
-        #     layerTree.addChildNode(l)
-        for (label, subtree) in self.newTree.items():
-            layers = self.treeToLayers(subtree)
-            if layers:
-                grp = layerTree.addGroup(label)
-                for l in layers:
-                    grp.addChildNode(l)
-        # removes layers !!
+        ll = self.treeToLayers(self.newTree)
+        for l in ll:
+            layerTree.addChildNode(l)
+
+        # caution: commented instruction below removes all layers !!
         # iface.layerTreeCanvasBridge().rootGroup().clear()
         layerTree.removeChildren(0, oldLen)
 
@@ -127,32 +102,18 @@ class MainPlugin(object):
             destination.append(layer)
 
     def treeToLayers(self, sourceTree):
-        groupContents = []
         if isinstance(sourceTree, dict):
+            groupContents = []
             for (layerType, layers) in sourceTree.items():
                 groupLayers = self.treeToLayers(layers)
                 if groupLayers:
                     grp = QgsLayerTreeGroup(layerType)
-                    for l in layers:
-                        grp.addLayer(l)
+                    for l in groupLayers:
+                        grp.addChildNode(l)
                     groupContents.append(grp)
             return groupContents
         elif isinstance(sourceTree, list):
-            return sourceTree
+            return [QgsLayerTreeLayer(l) for l in sourceTree]
         else:
             raise Exception("Tree dictionary has been initialized incorrectly.")
  
-
-# for el in oldTree.children():
-#     if QgsLayerTree.isLayer(el):
-#         iface.layerTreeCanvasBridge().rootGroup().addLayer(el.layer())
-#     elif QgsLayerTree.isGroup(el):
-#         iface.layerTreeCanvasBridge().rootGroup().addGroup(el.name())
-
-# oldTree = layerTree.clone()
-
-# for el in oldTree.children():
-#     if QgsLayerTree.isLayer(el):
-#         print(el.layer())
-#     elif QgsLayerTree.isGroup(el):
-#         print(el.name())
